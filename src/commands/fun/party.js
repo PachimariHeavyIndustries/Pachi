83fcc8d41be130a24b16992e079ddae38069d5a4
dolphini13 <munarokza@gmail.com>
2017-11-24 22:50:34 +1100

    dynamic user name ig?
    yeh

--- src/commands/fun/party.js
+++ src/commands/fun/party.js
@@ -1,7 +1,10 @@
 import Promise from 'bluebird';
+//ngl not sure if we need nconf or ramda rn but oh well
+import nconf from 'nconf';
+import R from 'ramda';  
 
-function party() {
-  return Promise.resolve(`Rawan calls for partying! \n"https://i.imgur.com/pVmW06p.gif"`);
+function party(client, evt, suffix) {
+  return Promise.resolve(`${evt.message.author.username} calls for partying! \n"https://i.imgur.com/pVmW06p.gif"`);
 }
 
 export default {
